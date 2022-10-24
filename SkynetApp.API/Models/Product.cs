b18e9ea1952ed05a9daaa4c9d46edf6e77a2b68c
ravikantpal1408 <ravikantpal123@gmail.com>
2022-10-24 11:59:43 -0700

    changes done

--- SkynetApp.API/Models/Product.cs
+++ SkynetApp.API/Models/Product.cs
@@ -3,6 +3,7 @@ namespace SkynetApp.API.Models;
 public class Product
 {
     public int Id { get; set; }
-    public string ProductName { get; set; }
+    public string Name { get; set; }
     public decimal Price { get; set; }
+
 }
