553f61f5c378cd481fb96ae0d814351090f47d2b
ravikantpal1408 <ravikantpal123@gmail.com>
2022-10-24 18:07:29 -0700

    modified the initial scripts

--- SkynetApp.API/Scripts/InitialDBSetup.sql
+++ SkynetApp.API/Scripts/InitialDBSetup.sql
@@ -2,9 +2,14 @@ CREATE DATABASE skinetDB;
 
 USE skinetDB;
 
-CREATE TABLE tblProduct
+CREATE TABLE [dbo].[tblProduct]
 (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    ProductName VARCHAR(100),
-    Price DECIMAL
+	[Id] BIGINT PRIMARY KEY IDENTITY(1,1),
+	[Name] VARCHAR(100),
+	[Description] VARCHAR(100),
+	[Price] DECIMAL,
+	[PictureUrl] VARCHAR(100),
+	[Type] VARCHAR(50),
+	[Brand] VARCHAR(50),
+	[QuantityInStock] INT
 );
--- SkynetApp.API/Scripts/InsertProduct.sql
+++ SkynetApp.API/Scripts/InsertProduct.sql
@@ -1,14 +1,8 @@
-USE skinetDB;
-GO 
-INSERT INTO tblProduct(ProductName, Price) 
-    VALUES('Product 1', 3.50);
-INSERT INTO tblProduct(ProductName, Price) 
-    VALUES('Product 2', 15.67);
-INSERT INTO tblProduct(ProductName, Price) 
-    VALUES('Product 3', 20.00);
-INSERT INTO tblProduct(ProductName, Price) 
-    VALUES('Product 4', 14.56);
-INSERT INTO tblProduct(ProductName, Price) 
-    VALUES('Product 5', 6.70);
-INSERT INTO tblProduct(ProductName, Price) 
-    VALUES('Product 6', 10.00);
+  USE [skinetDB];
+  GO
+  INSERT INTO [skinetDB].[dbo].[tblProduct] ([Name], [Description], [Price], [PictureUrl], [Type], [Brand], [QuantityInStock])
+  VALUES('Product 1', 'Description about product one', 10.20, '/somedir/some1.jpg', 'Type 1' , 'Brand 1', 10),
+  ('Product 2', 'Description about product two', 20.21, '/somedir/some2.jpg', 'Type 2' , 'Brand 2', 200),
+  ('Product 3', 'Description about product three', 40.99, '/somedir/some3.jpg', 'Type 3' , 'Brand 3', 100),
+  ('Product 4', 'Description about product four', 6.97, '/somedir/some4.jpg', 'Type 4' , 'Brand 4', 33),
+  ('Product 5', 'Description about product five', 2.99, '/somedir/some5.jpg', 'Type 5' , 'Brand 5', 939);
