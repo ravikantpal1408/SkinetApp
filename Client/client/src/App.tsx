b4733f12f133c9c848cce57b976a33ffbd578472
ravikantpal1408 <ravikantpal123@gmail.com>
2022-11-01 08:37:54 +0530

    refactored to build things from scratch

--- Client/client/src/App.tsx
+++ Client/client/src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from './Component/Button/Button';
+// import logo from './logo.svg';
+// import Button from './Component/Button/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>Hello there we have strated React Building block of app</h1>
-        <Button/>
-      </header>
+      <h1>Hello there we have strated React Building block of app</h1>
     </div>
   );
 }
