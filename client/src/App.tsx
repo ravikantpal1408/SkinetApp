1107358ba005746a5f4b0ffbbf8e503042a1a8c1
Ravi Kant Pal <35272306+ravikantpal1408@users.noreply.github.com>
2022-11-01 22:49:29 +0530

    Update App.tsx
    changes for app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>This is Skinet App </h1>
-    </div>
-  );
-}
+class App extends React.Component {
+    // constructor(props) {
+    //     super();
+    // }
+    render()
+    {
+        return (
+            <div className="App">
+                <h1>This is Skinet App </h1>
+                <button onClick={()=>alert('hello')}>Hello</button>
+            </div>
+        );
+    }
+} 
 
 export default App;
