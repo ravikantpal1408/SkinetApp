fb43b506e772708b2aed82338c665e2adf1012b2
ravikantpal1408 <ravikantpal123@gmail.com>
2022-11-01 22:59:07 +0530

    removed client project for sake

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/This is Skinet App/i);
-  expect(linkElement).toBeInTheDocument();
-});
