47b792c56a8d62579ffd2cd0377b51597476c76d
ravikantpal1408 <ravikantpal123@gmail.com>
2022-11-02 08:27:45 +0530

    demo calling product API from client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
+﻿import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  componentDidMount(): void {
+    fetch(`https://localhost:44396/api/Product/products`)
+      .then(res => {console.log(res)})
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <h1>Hello there skinet 😊😊</h1>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
