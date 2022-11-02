46d9ed6d4e00e9f64e809b8c1c4ef6471b5f5c6c
Ravi Kant Pal <35272306+ravikantpal1408@users.noreply.github.com>
2022-11-02 15:15:22 +0530

    added contructor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello there 😎</h1>
-    </div>
-  );
+
+interface AppState
+{
+    data : any,
+}
+class App extends React.Component<AppState> {
+    
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+    
+    
+    componentDidMount(){
+        console.log('😀')
+    }
+    
+    render(){
+        return (
+            <div className="App">
+                <h1>Hello there 😎</h1>
+            </div>
+        );
+    }
+  
 }
 
 export default App;
